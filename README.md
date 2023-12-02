0f7aa4f0077b0c677b8380888e31293d2ed50faf
brandogn <bwong928@berkeley.edu>
2023-12-02 13:17:26 -0800

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## Some Generative Art
+
+![Img1](Img1.png)
+
+![Img2](Img2.png)
+
+Feel free to run the script on your local machine by running `index.html` on a local server.
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const arrowLen = 1,
-  numFlowPts = 3,
+  numFlowPts = 5,
   colors = [7, 45, 70],
   canvW = 1900, // W = x
-  canvH = 750; // H = y
+  canvH = 600; // H = y
 
 const flowPoints = [];
 
@@ -17,8 +17,9 @@ function draw() {
 
   addFlowPoints(numFlowPts);
   // drawFlowGrid();
-  // drawCurves(10);
-  drawCurves(500);
+  drawCurves(1000);
+  // drawCurves(500);
+  // drawCurves(25);
   // drawCurves(50000);
   saveCanvas("Img", "png");
 }
@@ -42,7 +43,7 @@ function calcGravity(x, y) {
     // gravity = distance * (vector between the two points)
     const weight = dist(x, y, flowpt.x, flowpt.y);
     const weightedVec = createVector(flowpt.x - x, flowpt.y - y).mult(
-      (100 * arrowLen) / weight ** 1.5
+      100 / weight ** 1.5
     );
     gravVec.add(weightedVec);
   });
@@ -50,14 +51,31 @@ function calcGravity(x, y) {
 }
 
 function addFlowPoints(n) {
-  for (let i = 0; i < n; i += 1) {
-    const newPt = createVector(random(canvW), random(canvH));
+  const points = [
+    // [525.5938475106399, 569.2901246681279],
+    // [1243.6964113451947, 466.51327173388273],
+    // [1550.6474882391517, 239.39441861039361],
+    [-60, 200],
+    [1234, -70],
+    [1960, 600],
+  ];
+  points.forEach((pt) => {
+    const newPt = createVector(
+      pt[0] + random(-50, 50),
+      pt[1] + random(-50, 50)
+    );
     flowPoints.push(newPt);
-  }
+  });
+
+  // for (let i = 0; i < n; i += 1) {
+  //   const newPt = createVector(random(canvW), random(canvH));
+  //   flowPoints.push(newPt);
+  // }
 }
 
-function drawCurve(x, y, len) {
+function drawCurve(x, y, len, angle) {
   const tsteps = len * 10;
+  let flow, x_t, y_t, dx, dy;
   // half = tsteps / 2;
   push();
 
@@ -66,15 +84,27 @@ function drawCurve(x, y, len) {
   fill(0);
 
   // drawPoint(x, y);
+  x_t = x;
+  y_t = y;
   for (let i = 1; i < tsteps; i += 1) {
-    // half - abs(half - i)
-    // strokeWeight(((tsteps - abs(tsteps - (i*2))) ** 0.2) - 0.7);
-    const flow = getFlowVec(x, y).mult(0.1),
-      dx = flow.x,
-      dy = flow.y;
-    line(x, y, x + dx, y + dy);
-    x += dx;
-    y += dy;
+    // strokeWeight((tsteps - abs(tsteps - i * 2)) ** 0.1 - 1);
+    flow = getFlowVec(x_t, y_t).mult(0.1);
+    dx = flow.x;
+    dy = flow.y;
+    line(x_t, y_t, x_t + dx, y_t + dy);
+    x_t += dx;
+    y_t += dy;
+  }
+
+  x_t = x;
+  y_t = y;
+  for (let i = 1; i < tsteps; i += 1) {
+    flow = getFlowVec(x_t, y_t).mult(0.1);
+    dx = flow.x;
+    dy = flow.y;
+    line(x_t, y_t, x_t - dx, y_t - dy);
+    x_t -= dx;
+    y_t -= dy;
   }
 
   pop();
@@ -82,18 +112,11 @@ function drawCurve(x, y, len) {
 
 function drawCurves(n) {
   for (let i = 0; i < n; i += 1) {
-    // drawCurve(random(canvW), random(canvH), random(200, 300));
-    const pad = 100,
-      len = random(70, 100);
-    let x = random(-pad, canvW + pad),
-      y = random(-pad, canvH + pad);
+    const len = random(-20, 20),
+      x = random(canvW),
+      y = random(canvH);
 
-    // drawPoint(x,y)
-    drawCurve(x, y, len + 400);
-
-    // x = random(-pad, canvW + pad);
-    // y = random(-pad, canvH + pad);
-    // drawCurve(x, y, len - 50);
+    drawCurve(x, y, len + 808);
   }
 }
 
@@ -107,9 +130,32 @@ function getColor(x, y) {
     return color(res, 60, 100);
   } else if (t < randomGaussian(n)) {
     res = random(colors[1], colors[1] + 3);
-    return color(res, 40, 90);
+    return color(res, 40, 90);g
   } else {
     res = random(colors[2], colors[2] + 3);
     return color(res, 50, 90);
   }
 }
+
+
+/*
+ideas:
+jelly fish biolumniscent
+squid
+candles
+ion enginees
+clouds
+ice algae
+
+leafs
+birds
+water color
+thunder
+grain
+flames
+stars
+
+night vision, different computer vision
+
+*/
+
